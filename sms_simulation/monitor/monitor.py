c5e7fbfb2d372d20174e15652c4018e1e04c97a3
Jared <jcoughlin@dephy.com>
2024-08-23 14:55:57 -0400

    Adds producer and flushes out monitor class.

--- sms_simulation/monitor/monitor.py
+++ sms_simulation/monitor/monitor.py
@@ -1,5 +1,10 @@
 import argparse
+import multiprocessing as mp
+from typing import Dict
 from typing import List
+
+from sms_simulation.producer.producer import SmsProducer
+from sms_simulation.sender.sender import SmsSender
 
 
 # ============================================
@@ -16,8 +21,43 @@ class SmsMonitor:
         self._sendFailureRate: List[float] = args.sendFailureRate
         self._progUpdateTime: float = args.progUpdateTime
 
+        self._msgQueue: mp.Queue = mp.Queue(maxsize=self._nMessages)
+        self._responseQueue: mp.Queue = mp.Queue(maxsize=self._nMessages)
+
+        self._smsProducer: SmsProducer = SmsProducer(self._nMessages, self._msgQueue)
+        self._smsSenders: List[SmsSender] = [
+            SmsSender(
+                self._timeToSend[i],
+                self._sendFailureRate[i],
+                self._msgQueue,
+                self._responseQueue,
+            )
+            for i in range(self._nSenders)
+        ]
+
+        self._state: Dict[str, float] = {
+            "messagesSent": 0.0,
+            "successfulSends": 0.0,
+            "failedSends": 0.0,
+            "avgTimeToSend": 0.0,
+        }
+
     # -----
     # run
     # -----
     def run(self) -> int:
+        self._smsProducer.start()
+
+        for sender in self._smsSenders:
+            sender.start()
+
+        while self._state["messagesSent"] < self._nMessages:
+            self._monitor()
+
         return 0
+
+    # -----
+    # _monitor
+    # -----
+    def _monitor(self) -> None:
+        pass
---
+++ sms_simulation/producer/producer.py
@@ -0,0 +1,55 @@
+import multiprocessing as mp
+import random
+import string
+from typing import Dict
+
+
+# ============================================
+#                 SmsProducer
+# ============================================
+class SmsProducer:
+    # -----
+    # constructor
+    # -----
+    def __init__(self, nMessages: int, msgQueue: mp.Queue) -> None:
+        self._nMessages: int = nMessages
+        self._msgQueue: mp.Queue = msgQueue
+
+        self._maxMsgLen: int = 100
+
+        self._producerProcess = mp.Process(
+            target=self._produce_sms, args=(self._nMessages, self._msgQueue)
+        )
+
+    # -----
+    # _generate_phone_number
+    # -----
+    def _generate_phone_number(self) -> str:
+        phoneNumber: str = "-".join(
+            str(random.randint(0, 999)).zfill(3) for _ in range(3)
+        )
+        return phoneNumber + str(random.randint(0, 9))
+
+    # -----
+    # _generate_message
+    # -----
+    def _generate_message(self) -> str:
+        msgLen: int = random.randint(1, self._maxMsgLen)
+        return "".join(random.choice(string.ascii_lowercase) for _ in range(msgLen))
+
+    # -----
+    # _produce_sms
+    # -----
+    def _produce_sms(self, nMessages: int, msgQueue: mp.Queue) -> None:
+        for _ in range(nMessages):
+            sms: Dict[str, str] = {
+                self._generate_phone_number(): self._generate_message()
+            }
+
+            msgQueue.put(sms)
+
+    # -----
+    # start
+    # -----
+    def start(self) -> None:
+        self._producerProcess.start()
